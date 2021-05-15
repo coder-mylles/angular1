4ed6b82cf17617621795b1adbedad4137e9a84fc
martin-moringa <martin.mylles@student.moringachool.com>
2021-05-15 23:32:08 +0300

    add get values from pipe count

--- src/app/date-count.pipe.ts
+++ src/app/date-count.pipe.ts
@@ -1,3 +1,4 @@
+
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
@@ -5,8 +6,27 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class DateCountPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: any): number {
+    let today:Date = new Date();
+    let todayWithNoTime:any = new Date(today.getFullYear(), today.getMonth(), today.getDate())
+    var dateDifference = Math.abs(value - todayWithNoTime)
+    const secondsInDay = 86400;
+    const secondsInHour = 3600;
+    const hoursInDay = 24;
+    var dateDifferenceSeconds = dateDifference*0.001;
+    var dateCounter = dateDifferenceSeconds/secondsInDay;
+    var hourCounter = (dateCounter % 1);
+    var hours = hourCounter * hoursInDay;
+    var finalTime:any = Math.floor(dateCounter) +" day(s) " + hours + " hour(s) "  ;
+    
+
+
+
+    if (dateCounter >= 1 && value < todayWithNoTime){
+      return finalTime;
+    }else{
+      return 0;
+    }
   }
 
 }
